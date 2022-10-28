b2414f792ed0612592e669da319f6f36b9f7739d
Darshan Suthar <darshansu156@gmail.com>
2022-10-28 22:51:23 +0530

    added spinner for submit button

--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -7,9 +7,12 @@ interface AddItemModalProps {
 }
 
 const AddItemModal: FC<AddItemModalProps> = ({ setItems }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const { mutate: addItem } = trpc.item.createItem.useMutation({
     onSuccess: (shoppingItem) => {
       setItems((prev) => [...prev, shoppingItem]);
+      setLoading(false);
       setItemName("");
     },
   });
@@ -21,6 +24,7 @@ const AddItemModal: FC<AddItemModalProps> = ({ setItems }) => {
         onSubmit={(e) => {
           e.preventDefault();
           if (itemName.trim() !== "") {
+            setLoading(true);
             addItem({ name: itemName.trim() });
           }
         }}
@@ -36,12 +40,41 @@ const AddItemModal: FC<AddItemModalProps> = ({ setItems }) => {
             value={itemName}
           />
         </div>
-        <button
-          type="submit"
-          className="w-40 border-2 border-black py-1 px-5 font-bold transition  duration-150 ease-in-out hover:bg-black hover:text-white"
-        >
-          Submit
-        </button>
+
+        {loading ? (
+          <button
+            type="submit"
+            className="flex h-10 items-center justify-center border-2 border-black bg-black font-bold  text-white transition duration-150 ease-in-out"
+          >
+            <svg
+              className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="flex h-10 items-center justify-center border-2 border-black font-bold transition  duration-150 ease-in-out hover:bg-black hover:text-white"
+          >
+            Submit
+          </button>
+        )}
       </form>
     </div>
   );
