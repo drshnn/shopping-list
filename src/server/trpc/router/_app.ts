f8b7e563bea909079606912fcfa73ebfcfa95456
Darshan Suthar <darshansu156@gmail.com>
2022-10-28 11:00:41 +0530

    deleted example route

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,8 @@
 // src/server/router/_app.ts
 import { router } from "../trpc";
-
-import { exampleRouter } from "./example";
 import { itemRouter } from "./itemRouter";
 
 export const appRouter = router({
-  example: exampleRouter,
   item: itemRouter,
 });
 
