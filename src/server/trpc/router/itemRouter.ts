c0ce2af67e316eba72439de57eb53d3766585c9c
Darshan Suthar <darshansu156@gmail.com>
2022-10-28 23:00:20 +0530

    added checkedItem in backend

--- src/server/trpc/router/itemRouter.ts
+++ src/server/trpc/router/itemRouter.ts
@@ -37,4 +37,23 @@ export const itemRouter = router({
       });
       return item;
     }),
+  checkItem: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        checked: z.boolean(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { id, checked } = input;
+      const item = await ctx.prisma.shoppingItem.update({
+        where: {
+          id,
+        },
+        data: {
+          checked,
+        },
+      });
+      return item;
+    }),
 });
