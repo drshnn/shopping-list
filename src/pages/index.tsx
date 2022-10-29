c855350494535193c452d08ac26533cf0886529a
Darshan Suthar <darshansu156@gmail.com>
2022-10-29 20:36:31 +0530

    implemented button to clear the items list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { itemRouter } from "../server/trpc/router/itemRouter";
 
 const Home: NextPage = () => {
   const [isAddActive, setIsAddActive] = useState<boolean>(false);
+  const [isClearLoading, setIsClearLoading] = useState<boolean>(false);
   const [items, setItems] = useState<ShoppingItem[]>([]);
   const { data: itemsData, isLoading } = trpc.item.getItem.useQuery(
     {},
@@ -33,7 +34,12 @@ const Home: NextPage = () => {
       setItems(shoppingItem);
     },
   });
-
+  const { mutate: deleteAllItems } = trpc.item.deleteAllItems.useMutation({
+    onSuccess: (items) => {
+      setIsClearLoading(false);
+      setItems([]);
+    },
+  });
   if (!itemsData || isLoading) return <p>Loading...</p>;
   return (
     <>
@@ -59,7 +65,48 @@ const Home: NextPage = () => {
         </nav>
         <main className="mt-20 flex flex-col gap-5 px-10  py-5 sm:flex-row">
           <div className="flex-5 order-2 flex-grow sm:order-1">
-            <p className="mb-3 text-xl font-semibold">All Items</p>
+            <div className="flex w-full justify-between">
+              <p className="mb-3 text-xl font-semibold">All Items</p>
+              {isClearLoading ? (
+                <button
+                  className="flex h-8 w-20
+                 items-center justify-center border-2 border-black bg-black font-bold  text-white transition duration-150 ease-in-out"
+                >
+                  <svg
+                    className="h-5 w-5 animate-spin text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </button>
+              ) : (
+                <button
+                  className="flex h-8 w-20 items-center justify-center border-2 border-black bg-black  text-white transition duration-150 ease-in-out"
+                  onClick={() => {
+                    if (items.length !== 0) {
+                      setIsClearLoading(true);
+                      deleteAllItems({});
+                    }
+                  }}
+                >
+                  Clear All
+                </button>
+              )}
+            </div>
             <ul className="flex flex-col gap-3">
               {items.map((item) => (
                 <li
